3c53f1cd2ed7d4e2a3fa4350f43888bce830962e
Asif Kamal <asif_kamal@hotmail.com>
2019-11-30 18:04:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"] << "nala cat"
 end
 
 def add_element_to_start_of_array(array, element)
