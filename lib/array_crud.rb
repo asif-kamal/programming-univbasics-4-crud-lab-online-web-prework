fbca5fa5a3062607ce4f6799429228f2c681be2e
Asif Kamal <asif_kamal@hotmail.com>
2019-11-30 18:06:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  beatles = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"] 
-  beatles << "nala cat"
+  array = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"] 
+  array << "nala cat"
 end
 
 def add_element_to_start_of_array(array, element)
